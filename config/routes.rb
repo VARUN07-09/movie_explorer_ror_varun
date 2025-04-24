93b9fc1209663f5417ae048c24bd6b385ad2d25c
VARUN07-09 <varun91thakur@gmail.com>
2025-04-24 23:54:27 +0530

    Fix: bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root to: redirect('/api-docs')
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs' if Rails.env.development? 
 
